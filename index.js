2dac400f4ba28f995513a47af48ab2e92862c7b5
mohammed shabeeb vk <mashabeeb7912@gmail.com>
2025-09-27 16:34:45 +0530

    redux

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 import { createStore } from "https://cdn.skypack.dev/redux";
+
+function counter(value){
+    return{
+        type:"increment",
+        payload:value
+    }
+}
+function counterr(value){
+    return{
+        type:"decrement",
+        payload:value
+    }
+}
  const reducer = (prev= {value:0},action)=>{
     switch(action.type){
         case 'increment':
             return{
                 ...prev,
-                value:prev.value+1
+                value:prev.value+action.payload
             }
         case 'decrement':
             return{
                 ...prev,
-                value:prev.value-1
+                value:prev.value-action.payload
             }
             default:
                 return prev
@@ -25,8 +38,10 @@ import { createStore } from "https://cdn.skypack.dev/redux";
  })
 
   document.getElementById('inc').onclick=()=>{
-   store.dispatch({type:"increment"});
+//    store.dispatch({type:"increment"});
+store.dispatch(counter(5))
   }
   document.getElementById('dec').onclick=()=>{
-    store.dispatch({type:"decrement"});
+    // store.dispatch({type:"decrement"});
+store.dispatch(counterr(1))
   }
